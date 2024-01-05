ad794474298d36db6156bdbe1b1a05a346e21439
Tejas Gathadi <tejasgathadi@gmail.com>
2024-01-05 11:25:50 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import "colors";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to Database".bgMagenta.white);
+  })
+  .catch((err) => {
+    console.log(`Error Connecting to Database ${err}`.bgRed.white);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("App listening on port 3000!!!");
+  console.log("App listening on port 3000!!!".bgBlue.white);
 });
