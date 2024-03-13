e29e32484479b28c7baca9d7c4d84eeeb7135f12
Tejas Gathadi <tejasgathadi@gmail.com>
2024-03-13 17:22:26 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -17,7 +18,10 @@ mongoose
     console.log(`Error Connecting to Database ${err}`.bgRed.white);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
+
 app.use(express.json());
 
 app.use(cookieParser());
@@ -29,6 +33,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
